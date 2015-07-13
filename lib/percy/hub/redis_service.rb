eb6681399f5aa683115153d4eb83f5fb4324eb0b
Mike Fotinakis <mike@fotinakis.com>
2015-07-13 16:36:28 -0700

    Reduce connection churn by inheritting sockets.

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -8,6 +8,12 @@ module Percy
           host: ENV['REDIS_HOST'] || '127.0.0.1',
           port: Integer(ENV['REDIS_PORT'] || 6379),
           db: Integer(ENV['REDIS_DB'] || 0),
+
+          # NOTE: Reduce connection churn by not re-establishing new connections in forked children.
+          # This basically assumes that we do not re-use Hub objects from within forked processes,
+          # and instead create new Hub instances. REDIS CONNECTIONS WILL BECOME CORRUPT if we ever
+          # re-use Hub instances inside forked children.
+          inherit_socket: true,
         }
         options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
         @redis ||= Redis.new(options)
