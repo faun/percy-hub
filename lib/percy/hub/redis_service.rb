350c1cea9b27eff01882f4e57129ca09df0c9184
Mike Fotinakis <mike@fotinakis.com>
2015-07-13 17:42:10 -0700

    Attempt to tune TCP keepalives for redis and HAProxy.

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -9,11 +9,17 @@ module Percy
           port: Integer(ENV['REDIS_PORT'] || 6379),
           db: Integer(ENV['REDIS_DB'] || 0),
 
-          # NOTE: Reduce connection churn by not re-establishing new connections in forked children.
-          # This basically assumes that we do not re-use Hub objects from within forked processes,
-          # and instead create new Hub instances. REDIS CONNECTIONS WILL BECOME CORRUPT if we ever
-          # re-use Hub instances inside forked children.
-          inherit_socket: true,
+          # By default, Redis TCP connections do NOT send keepalives. Since we connect to HAProxy
+          # instead of a Redis server directly, we need to tune TCP keepalives so that HAProxy
+          # can know when to reap connections. These are coordinated with the haproxy.cfg settings.
+          #
+          # Set Redis TCP connections to send the first keepalive after 5 seconds of inactivity
+          # and every 5 seconds thereafter, and 2 unacknowledged probes will kill the connection.
+          tcp_keepalive: {
+            time: 5,
+            intvl: 5,
+            probes: 2,
+          },
         }
         options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
         @redis ||= Redis.new(options)
