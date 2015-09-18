84e3b0ec254d0d47bc8647354cb8b830aca22cf5
Mike Fotinakis <mike@fotinakis.com>
2015-09-18 16:56:30 -0700

    Attempt to fix redis-rb lack of connection cleanup issues.

--- spec/percy/hub/redis_service_spec.rb
+++ spec/percy/hub/redis_service_spec.rb
@@ -7,10 +7,13 @@ RSpec.describe Percy::Hub::RedisService do
     end
   end
   describe '#reset_redis_connection' do
-    it 'clears the instance variable' do
+    it 'clears the instance variable and disconnects ' do
       old_redis = hub.redis
+      old_redis.get('test')
+      expect(old_redis.connected?).to eq(true)
       hub.reset_redis_connection
       expect(hub.redis).to_not eq(old_redis)
+      expect(old_redis.connected?).to eq(false)
     end
   end
 end
