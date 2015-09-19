f6eb04675f02a14e1801061ec6694d45bfd335b4
Mike Fotinakis <mike@fotinakis.com>
2015-09-18 17:19:34 -0700

    Revert "Attempt to fix redis-rb lack of connection cleanup issues."
    This reverts commit 84e3b0ec254d0d47bc8647354cb8b830aca22cf5.

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -4,8 +4,6 @@ module Percy
   class Hub
     module RedisService
       def redis
-        return @redis if @redis
-
         options = {
           host: ENV['REDIS_HOST'] || '127.0.0.1',
           port: Integer(ENV['REDIS_PORT'] || 6379),
@@ -13,25 +11,13 @@ module Percy
         }
         options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
 
-        @redis = Redis.new(options)
-
-        # Important: define a finalizer that will close the redis TCP connection when this object
-        # gets garbage collected. It is also important that this proc is from the class method
-        # and not just created inline here, see:
-        # http://www.mikeperham.com/2010/02/24/the-trouble-with-ruby-finalizers/
-        ObjectSpace.define_finalizer(@redis, self.class._destroy_redis_client(@redis))
-
-        @redis
+        @redis ||= Redis.new(options)
       end
 
       def reset_redis_connection
         @redis.disconnect! if @redis
         @redis = nil
       end
-    end
-
-    def self._destroy_redis_client(redis_obj)
-      proc { redis_obj.disconnect! }
     end
   end
 end
