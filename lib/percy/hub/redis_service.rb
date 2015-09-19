69e13524a5837ff50105f0c9b27c56060d60295c
Mike Fotinakis <mike@fotinakis.com>
2015-09-18 17:19:42 -0700

    Revert "Remove TCP keep-alive-ness."
    This reverts commit a8173eddc4b3ff2992796e5d080b2a6042292e23.

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -8,6 +8,18 @@ module Percy
           host: ENV['REDIS_HOST'] || '127.0.0.1',
           port: Integer(ENV['REDIS_PORT'] || 6379),
           db: Integer(ENV['REDIS_DB'] || 0),
+
+          # By default, Redis TCP connections do NOT send keepalives. Since we connect to HAProxy
+          # instead of a Redis server directly, we need to tune TCP keepalives so that HAProxy
+          # can know when to reap connections. These are coordinated with the haproxy.cfg settings.
+          #
+          # Set Redis TCP connections to send the first keepalive after 4 seconds of inactivity
+          # and every 4 seconds thereafter, and 4 unacknowledged probes will kill the connection.
+          tcp_keepalive: {
+            time: 4,
+            intvl: 4,
+            probes: 4,
+          },
         }
         options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
 
