a0dc1168b400c7df7c1406848b76c48e63fcb323
Mike Fotinakis <mike@fotinakis.com>
2016-02-08 09:59:53 -0800

    Better timeout defaults for redis.

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -8,7 +8,11 @@ module Percy
           host: ENV['REDIS_HOST'] || '127.0.0.1',
           port: Integer(ENV['REDIS_PORT'] || 6379),
           db: Integer(ENV['REDIS_DB'] || 0),
-          reconnect_attempts: 3,
+
+          # Don't wait forever if our Redis instance is down. Defaults are 5, 5, and 1.
+          timeout: 3,
+          connect_timeout: 3,
+          reconnect_attempts: 2,
         }
         options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
 
