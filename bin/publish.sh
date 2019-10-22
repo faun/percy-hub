1d5755f7ff61a862c2c05aae5dcc8155a165a133
Mike Fotinakis <mike@fotinakis.com>
2019-10-22 16:48:31 -0400

    Update publish script.

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,7 +3,11 @@
 # Make sure the current working dir is the same as the script that was run.
 cd "$(dirname "$0")" || exit 1
 CURDIR=$(pwd)
-NOW=`date +%Y-%m-%d-%H%M%S`
+
+[ -z "$BUILDKITE_COMMIT" ] && echo "[ERROR] Need to set BUILDKITE_COMMIT environment variable." && exit 1
+
+TAG="$BUILDKITE_COMMIT"
+echo "Building: $IMAGE:$TAG"
 
 function safe_git_pull_origin_master {
   if [ -n "$(git status --porcelain)" ]; then
@@ -15,16 +19,16 @@ function safe_git_pull_origin_master {
 
 
 IMAGE="gcr.io/percy-prod/hub"
-echo "Building $IMAGE"
 cd "$CURDIR/.." && safe_git_pull_origin_master && cd "$CURDIR/.." || exit
-docker build -f Dockerfile -t "$IMAGE:$NOW" ./
-docker tag "$IMAGE:$NOW" "$IMAGE:latest"
-gcloud docker -- push "$IMAGE:$NOW"
-gcloud docker -- push "$IMAGE:latest"
+gcloud auth configure-docker
+docker build -f Dockerfile -t "$IMAGE:$TAG" ./
+docker tag "$IMAGE:$TAG" "$IMAGE:latest"
+docker push "$IMAGE:$TAG"
+docker push "$IMAGE:latest"
 
 # Copy image to percy-dev registry.
 docker tag "$IMAGE:latest" gcr.io/percy-dev/hub:latest
-gcloud docker -- push gcr.io/percy-dev/hub:latest
+docker push gcr.io/percy-dev/hub:latest
 
 echo
-echo "Successfully built image $IMAGE with tags '$NOW' and 'latest'."
+echo "Successfully built image $IMAGE with tags '$TAG' and 'latest'."
