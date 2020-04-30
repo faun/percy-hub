7380a7fb6dcd2ff1ac4d2dee53a7f114eef993d7
Faun <git@faun.me>
2020-04-29 17:22:07 -0700

    Give REDIS_URL configuration precedence

--- lib/percy/hub/redis_service.rb
+++ lib/percy/hub/redis_service.rb
@@ -1,14 +1,21 @@
 require 'redis'
+require 'percy/redis_client'
 
 module Percy
   class Hub
     module RedisService
-      def redis
-        options = {
-          host: ENV['REDIS_HOST'] || '127.0.0.1',
-          port: Integer(ENV['REDIS_PORT'] || 6379),
-          db: Integer(ENV['REDIS_DB'] || 0),
+      def redis(options = {})
+        @redis ||= redis_client(default_redis_options.merge(options)).client
+      end
 
+      def disconnect_redis
+        @redis&.close
+        @redis = nil
+        @redis_client = nil
+      end
+
+      private def default_redis_options
+        options = {
           # These need to be longer than the longest BRPOPLPUSH timeout.
           # Defaults are 5, 5, and 1.
           timeout: 20,
@@ -16,14 +23,19 @@ module Percy
           reconnect_attempts: 2,
           tcp_keepalive: 10,
         }
-        options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
-
-        @redis ||= Redis.new(options)
+        if ENV['REDIS_URL']
+          options[:url] = ENV['REDIS_URL'] if ENV['REDIS_URL']
+        else
+          options[:host] = ENV['REDIS_HOST'] if ENV['REDIS_HOST']
+          options[:port] = Integer(ENV['REDIS_PORT']) if ENV['REDIS_PORT']
+          options[:db] = Integer(ENV['REDIS_DB']) if ENV['REDIS_DB']
+          options[:password] = ENV['REDIS_PASSWORD'] if ENV['REDIS_PASSWORD']
+        end
+        options
       end
 
-      def disconnect_redis
-        @redis&.close
-        @redis = nil
+      private def redis_client(options)
+        @redis_client ||= Percy::RedisClient.new(options)
       end
     end
   end
--- spec/percy/hub/redis_service_spec.rb
+++ spec/percy/hub/redis_service_spec.rb
@@ -5,6 +5,70 @@ RSpec.describe Percy::Hub::RedisService do
     it 'returns a client' do
       expect(hub.redis).to be
     end
+
+    context 'without any environment variables' do
+      let(:client_connection) { hub.redis.connection }
+      let(:host) { ENV['REDIS_HOST'] || '127.0.0.1' }
+      let(:port) { 6379 }
+      let(:db) { 7 }
+
+      it 'has the default configuration' do
+        expect(client_connection.dig(:host)).to eq(host)
+        expect(client_connection.dig(:port)).to eq(port)
+        expect(client_connection.dig(:db)).to eq(db)
+        expect(client_connection.dig(:location)).to eq("#{host}:#{port}")
+      end
+    end
+
+    context 'with legacy redis environment variables' do
+      around(:each) do |ex|
+        original_redis_host = ENV['REDIS_HOST']
+        original_redis_db = ENV['REDIS_DB']
+        original_redis_port = ENV['REDIS_PORT']
+        ENV['REDIS_HOST'] = host
+        ENV['REDIS_DB'] = db.to_s
+        ENV['REDIS_PORT'] = port.to_s
+        ex.run
+        ENV['REDIS_HOST'] = original_redis_host
+        ENV['REDIS_DB'] = original_redis_db
+        ENV['REDIS_PORT'] = original_redis_port
+      end
+
+      let(:host) { ENV['REDIS_HOST'] || 'localhost' }
+      let(:port) { 6379 }
+      let(:db) { 10 }
+      let(:client_connection) { hub.redis.connection }
+
+      it 'has the correct configuration' do
+        expect(client_connection.dig(:host)).to eq(host)
+        expect(client_connection.dig(:port)).to eq(port)
+        expect(client_connection.dig(:db)).to eq(db)
+        expect(client_connection.dig(:location)).to eq("#{host}:#{port}")
+      end
+    end
+
+    context 'with REDIS_URL environment variable' do
+      around(:each) do |ex|
+        original_redis_url = ENV['REDIS_URL']
+        ENV['REDIS_URL'] = redis_url
+        ex.run
+        ENV['REDIS_URL'] = original_redis_url
+      end
+
+      let(:scheme) { 'redis://' }
+      let(:host) { ENV['REDIS_HOST'] || 'localhost' }
+      let(:port) { 6379 }
+      let(:db) { 10 }
+      let(:redis_url) { "#{scheme}#{host}:#{port}/#{db}" }
+      let(:client_connection) { hub.redis.connection }
+
+      it 'has the correct configuration' do
+        expect(client_connection.dig(:host)).to eq(host)
+        expect(client_connection.dig(:port)).to eq(port)
+        expect(client_connection.dig(:db)).to eq(db)
+        expect(client_connection.dig(:location)).to eq("#{host}:#{port}")
+      end
+    end
   end
 
   describe '#disconnect_redis' do
