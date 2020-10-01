e1a854ad30cb6112ddcb341ce8c3243bc5cb15ba
Faun <git@faun.me>
2020-10-01 12:16:17 -0700

    WIP

--- lib/percy/hub/honeycomb_client.rb
+++ lib/percy/hub/honeycomb_client.rb
@@ -2,11 +2,13 @@ require 'honeycomb-beeline'
 
 module Percy
   class Hub
-    HoneycombClient = ::Honeycomb.dup
-    HoneycombClient.configure do |config|
+    Honeycomb = ::Honeycomb.dup
+    Honeycomb.configure do |config|
       config.write_key = ENV['HONEYCOMB_WRITE_KEY']
       config.dataset = ENV['HONEYCOMB_DATASET']
       config.service_name = 'percy-hub'
     end
+
+    ::Redis.honeycomb_client = Honeycomb.client
   end
 end
